3dbce5264017d8abbfc2f95c9533955fc74e4a29
Donghyun Son <69744314+stat-thon@users.noreply.github.com>
2024-05-02 15:02:08 +0900

    update py

--- Thon/Baekjoon/Week4/02_치킨_배달.py
+++ Thon/Baekjoon/Week4/02_치킨_배달.py
@@ -37,3 +37,52 @@ for coords in combinations(chicken, M):
     MIN = min(MIN, total_dist)
     
 print(MIN)
+
+
+### 답안 비교
+ans = 1e9
+def solve():
+    global ans
+    N, M = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(N)]
+    houses = []
+    chickens = []
+    chicken_dists = []
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == 1:
+                houses.append((i, j))
+            elif board[i][j] == 2:
+                chickens.append((i, j))
+
+
+    for r1, c1 in houses:
+        chicken_dists.append([])
+        for idx, (r2, c2) in enumerate(chickens):
+            chicken_dists[-1].append((abs(r1 - r2) + abs(c1 - c2), idx))
+        chicken_dists[-1].sort(key=lambda x:x[0])
+
+    ans = 1e9
+    visited = [False for _ in range(len(chickens))]
+    def dfs(depth, start):
+        global ans
+        if depth == M:
+            tmp = 0
+            for D in chicken_dists:
+                for distance, idx in D:
+                    if visited[idx]:
+                        tmp += distance
+                        break
+            ans = min(ans, tmp)
+            return
+
+        for i in range(start, len(chickens)):
+            if not visited[i]:
+                visited[i] = True
+                dfs(depth + 1, i + 1)
+                visited[i] = False
+
+    dfs(0, 0)
+    print(ans)
+
+solve()
