f65c4c48fa130dea5d9a1eed288606fcfbc8ad46
Donghyun Son <69744314+stat-thon@users.noreply.github.com>
2024-05-11 16:52:16 +0900

    update py

--- Thon/Baekjoon/Week5/02_정수_삼각형.py
+++ Thon/Baekjoon/Week5/02_정수_삼각형.py
@@ -31,3 +31,29 @@ else:
         result = cnt
                 
     print(max(result))
+
+
+
+### 비교 답안
+# 1932  정수 삼각형 (파이썬 Python)
+n = int(input())
+dp = []
+
+for i in range(n) :                            ## 입력값 이차원리스트 형태로 dp테이블에 저장하기
+    dp.append(list(map(int,input().split())))
+
+print(dp)
+print(dp[1][0])
+
+for i in range(1,n) :                           ## 행을 기준으로 for문 구성
+    for j in range(0,i+1) :                     ## 열을 기준으로 for문 구성
+        if j == 0 :
+            dp[i][0] += dp[i-1][0]              # 0열끼리 더하기
+        elif j == i :
+            dp[i][j] += dp[i-1][j-1]            # 마지막 열끼리 더하기
+        else :
+            dp[i][j] += max(dp[i-1][j-1],dp[i-1][j])    # 두 화살표중 더 큰 경우 받아들이기
+
+print(max(dp[n-1])) 
+
+# 출처: https://animoto1.tistory.com/entry/%EB%B0%B1%EC%A4%80-1932-%EC%A0%95%EC%88%98-%EC%82%BC%EA%B0%81%ED%98%95-%ED%8C%8C%EC%9D%B4%EC%8D%AC-Python
