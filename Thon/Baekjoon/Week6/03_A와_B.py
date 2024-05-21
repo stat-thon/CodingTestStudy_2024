9c04aa08bf21ed683b66efa89ecbb201afaae612
thon <stat.thon@gmail.com>
2024-05-21 13:40:41 +0900

    upload py

--- Thon/Baekjoon/Week6/03_A와_B.py
+++ Thon/Baekjoon/Week6/03_A와_B.py
@@ -1,4 +1,16 @@
 ### 12904 - A와 B
-# 고민:
-# 코딩:
+# 고민: 20분
+# 코딩: 포기
 
+S = list(input())
+T = list(input())
+
+while len(T) != len(S):
+    q = T.pop()
+    if q == 'B':
+        T = T[::-1]
+
+if T == S:
+    print(1)
+else:
+    print(0)
