0378cac9029c3361f7c69ddd4bf49bfc11867afb
thon <stat.thon@gmail.com>
2024-06-20 14:03:45 +0900

    update readme

--- Thon/Baekjoon/Week6/04_키_순서.py
+++ Thon/Baekjoon/Week6/04_키_순서.py
@@ -2,71 +2,92 @@
 # 고민: 40분
 # 코딩: 포기
 
+# 풀이 1 - PyPy 컴파일러
 # 플로이드-워셜 알고리즘
 import sys
+input = sys.stdin.readline
+'''
+플로이드-워셜 알고리즘은 임의의 노드 s에서 e까지 가는 데 걸리는 최단거리를 구하기 위해,
+s와 e 사이의 노드인 m에 대해 s에서 m까지 가는 데 걸리는 최단거리와 m에서 e까지 가는 데 걸리는 최단거리를 이용한다.
+시간복잡도는 O(V^3)
+'''
 
-#입력
+# N : 학생들의 수(=노드 갯수), M : 비교 횟수(=간선 갯수)
 N, M = map(int, input().split())
-height = [[0 for _ in range(N+1)] for _ in range(N+1)]
 
-for _ in range(M):
-    tall, short = map(int, sys.stdin.readline().split())
-    height[tall][short] = 1
+# 1. 그래프 초기화
+MAX = 1e9
+graph = [[MAX for _ in range(N + 1)] for _ in range(N + 1)]
 
-#플로이드 와샬 알고리즘
-for k in range(1, N+1): #경로 for문이 가장 상위 단계여야 누락되지 않는다
-    for i in range(1, N+1):
-        for j in range(1, N+1):
-            if height[i][j] == 1 or (height[i][k] ==1 and height[k][j] == 1):
-                height[i][j] = 1 #자신보다 작은 경우
+# 2. 입력
+for i in range(M):
+    a, b = map(int, input().split())   # a < b
+    graph[a][b] = 1
+
+# 3. 플로이드 워셜 진행
+for m in range(1, N + 1):  # 중간 지점
+    for s in range(1, N + 1):   # 시작
+        for e in range(1, N + 1):   # 끝
+            # 시작 ~ 마지막 > 시작 ~ 가운데 + 가운데 + 끝 -> 갱신
+            if graph[s][e] > graph[s][m] + graph[m][e]:
+                graph[s][e] = graph[s][m] + graph[m][e]
 
 
-#출력
+# 4. 모든 학생과의 비교가 가능한 경우
+#      == 거리가 INF가 아닌 학생의 수가 N-1인 경우
 answer = 0
-for i in range(1, N+1):
-    known_height = 0
+for i in range(1, N + 1):
+    cnt = 0
     for j in range(1, N+1):
-        known_height += height[i][j] + height[j][i] #자신보다 작은사람과 큰사람의 합
-    if known_height == N-1: #자신의 키 순서를 알 경우
+        if graph[i][j] != MAX or graph[j][i] != MAX:
+            cnt += 1
+
+    if cnt == N-1:
         answer += 1
+
 print(answer)
 
 
 
-### 풀이 2
-from collections import defaultdict
+
+
+### 풀이 2 - Python3 컴파일러인데 시간도 빠름
 import sys
 input = sys.stdin.readline
 
+def dfs(now, visit, graph):
+    visit[now] = 1
+    for nxt in graph[now]:
+        if visit[nxt]:
+            continue
+        dfs(nxt, visit, graph)
 
-def dfs(start, currNode, visited):
-    visited[currNode] = True
-    for nextNode in graph[currNode]:
-        if not visited[nextNode]:
-            height[start].add(nextNode)
-            height[nextNode].add(start)
-            dfs(start, nextNode,visited)
+def solution():
+    
+    # input 받고
+    N, M = map(int, input().split())
+    graph = [[] for _ in range(N + 1)]
+    
+    for _ in range(M):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+    
+    # visited 만들고
+    visited = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+    
+    # dfs 돌리기
+    for i in range(1, N + 1):
+        dfs(i, visited[i], graph)
 
+    result = 0
+    for i in range(1, N + 1):
+        cnt = 0
+        for j in range(1, N + 1):
+            if visited[i][j] or visited[j][i]:
+               cnt += 1
+        if cnt == N:
+            result += 1
 
-# 각 학생별로 자신보다 키가 큰 사람, 작은 사람을 저장하는 딕셔너리
-height = defaultdict(set)
-answer = 0
-n, m = map(int, input().split())
+    print(result)
 
-# 키가 큰 사람에서 작은 방향으로 향하는 간선 그래프
-graph = [[] for _ in range(n+1)] 
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-
-# 자신을 시작점으로 깊이 우선 탐색
-# 시작점에서 출발하면서 거친 모든 노드는 시작점보다 키가 작다.
-for i in range(1, n+1):
-    visited = [False] * (n+1)
-    dfs(i, i, visited)
-
-# 자신보다 키가 큰 사람 + 작은 사람의 정보가 N-1명이면 가능
-for i in range(1, n+1):
-    if len(height[i]) == n-1:
-        answer += 1
-print(answer)
+solution()
