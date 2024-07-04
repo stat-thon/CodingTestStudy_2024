3eaf0080fe361211153da51c8a0eba77ea9368ff
thon <stat.thon@gmail.com>
2024-07-04 11:20:55 +0900

    update py

--- Thon/Baekjoon/Week7/03_주사위_굴리기.py
+++ Thon/Baekjoon/Week7/03_주사위_굴리기.py
@@ -59,3 +59,52 @@ for d in moves:
     result = move(d)
     if result != 'no':
         print(result)
+        
+        
+### 코드 단순화
+
+import sys
+input = sys.stdin.readline
+
+N, M, x, y, K = map(int, input().split())
+dice_map = []
+
+for _ in range(N):
+    dice_map.append(list(map(int, input().split())))
+
+moves = list(map(int, input().split()))
+
+dice = [0] * 6
+
+def move_dice(d):
+    if d == 1:
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = dice[0], dice[5], dice[1], dice[2], dice[4], dice[3]
+    elif d == 2:
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = dice[0], dice[2], dice[3], dice[5], dice[4], dice[1]
+    elif d == 3:
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = dice[2], dice[1], dice[4], dice[3], dice[5], dice[0]
+    else:
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = dice[5], dice[1], dice[0], dice[3], dice[2], dice[4]
+
+
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+
+for d in moves:
+    
+    nx = x + dx[d - 1]
+    ny = y + dy[d - 1]
+    
+    if 0 <= nx < N and 0 <= ny < M:
+        x, y = nx, ny
+        
+        move_dice(d)
+
+        if dice_map[x][y] == 0:
+            dice_map[x][y] = dice[5]
+            
+        else:
+            dice[5] = dice_map[x][y]
+            dice_map[x][y] = 0 # 이걸 안 해줘서 계속 틀렸었음..
+
+        print(dice[2])
