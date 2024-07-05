311f6a1185a3d36dba7dc1cb4716507971bff178
thon <stat.thon@gmail.com>
2024-07-05 15:40:03 +0900

    update py

--- Thon/Baekjoon/Week7/04_미세먼지_안녕!.py
+++ Thon/Baekjoon/Week7/04_미세먼지_안녕!.py
@@ -103,3 +103,104 @@ for r in range(R):
     result += sum(room[r])
 
 print(result)
+
+
+### 큐로 구현 (근데 느림)
+import sys
+from collections import deque
+# input = sys.stdin.readline
+
+R, C, T = map(int, input().split())
+
+room = []
+
+for _ in range(R):
+    room.append(deque(map(int, input().split())))
+
+for r in range(R):
+    if room[r][0] == -1:
+        air_u = r
+        break
+
+air_d = air_u + 1
+
+def air_circulator():
+    
+    ## upper
+    # ↓
+    for i in range(air_u - 1, 0, -1):
+        room[i][0] = room[i - 1][0]
+
+    # ←
+    room[0].popleft()
+
+    # ↑
+    room[0].append(room[1][-1])
+    
+    for i in range(1, air_u):
+        room[i][-1] = room[i + 1][-1]
+
+    # →
+    room[air_u].pop()
+    room[air_u].insert(1, 0)
+
+    ## lower
+    # ↑
+    for i in range(air_d + 1, R - 1):
+        room[i][0] = room[i + 1][0]
+
+    # ←
+    room[R - 1].popleft()
+
+    # ↓
+    room[R - 1].append(room[R - 2][-1])
+    
+    for i in range(R - 2, air_d, -1):
+        room[i][-1] = room[i - 1][-1]
+
+    # →
+    room[air_d].pop()
+    room[air_d].insert(1, 0)
+    
+
+# 시뮬레이션
+nx = [-1, 1, 0, 0]
+ny = [0, 0, -1, 1]
+
+for _ in range(T):
+    new_room = [deque([0] * C) for _ in range(R)]
+
+    # 1. 확산
+    for r in range(R):
+        for c in range(C):
+            
+            if room[r][c] >= 5: # 확산 가능한 좌표
+                
+                minus = 0 # 기존 방에 남은 양을 계산하기 위함
+                for d in range(4): # 4방향
+                    x = r + nx[d]
+                    y = c + ny[d]
+                    
+                    dirt = int(room[r][c] / 5) # 확산량
+                    
+                    if 0 <= x < R and 0 <= y < C and (x, y) not in ((air_d, 0), (air_u, 0)):
+                        minus += dirt
+                        new_room[x][y] += dirt # 확산
+
+                # 기존 방에 남은 양 확산 후 남은 양 더함
+                new_room[r][c] += (room[r][c] - minus)
+            elif room[r][c] == -1:
+                new_room[r][c] = room[r][c]
+            else:
+                new_room[r][c] += room[r][c]
+    room = new_room
+    
+    # 2. 공기 청정
+    air_circulator()
+    
+
+result = 2
+for r in range(R):
+    result += sum(room[r])
+
+print(result)
