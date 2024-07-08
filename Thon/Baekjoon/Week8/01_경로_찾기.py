5abd2ffacf496abca271c2c51dd35b3f3e120662
thon <stat.thon@gmail.com>
2024-07-08 11:06:16 +0900

    update py

---
+++ Thon/Baekjoon/Week8/01_경로_찾기.py
@@ -0,0 +1,32 @@
+### 경로 찾기
+# 고민: 10분
+# 코딩: 20분
+
+import sys
+input = sys.stdin.readline
+
+N = int(input()) # node 수
+
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, input().split())))
+
+result = [[0] * N for _ in range(N)]
+
+def dfs(i, n):
+    
+    for now in range(N):
+
+        if visited[now] == 0 and graph[n][now] == 1:
+            result[i][now] = 1
+            visited[now] = 1
+            dfs(i, now)
+
+    return
+
+for i in range(N):
+    visited = [0] * N
+    dfs(i, i)
+    
+for i in range(N):
+    print(*result[i])
