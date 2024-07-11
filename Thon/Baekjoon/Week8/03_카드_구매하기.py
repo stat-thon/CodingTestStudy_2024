ee9392a3a2ea03a8cac3d4f6a39f40bd184a4766
Donghyun Son <69744314+stat-thon@users.noreply.github.com>
2024-07-11 14:55:16 +0900

    upload py

--- Thon/Baekjoon/Week8/03_카드_구매하기.py
+++ Thon/Baekjoon/Week8/03_카드_구매하기.py
@@ -1,4 +1,29 @@
 ### 11052 - 카드 구매하기
-# 고민: 
-# 코딩: 
+# 고민: 60분
+# 코딩: 20분
 
+N = int(input())
+prices = list(map(int, input().split()))
+
+def solution():
+    dp = [0] * N
+    dp[0] = prices[0]
+
+    if N == 1:
+        return dp[0]
+    
+    dp[1] = max(dp[0] + prices[0], prices[1])
+    
+    if N == 2:
+        return dp[1]
+    
+    for i in range(2, N):
+        MAX = prices[i]
+        for j in range(1, i):
+            MAX = max(dp[i - j] + prices[j - 1], MAX)
+
+        dp[i] = MAX
+        
+    return dp[-1]
+
+print(solution())
