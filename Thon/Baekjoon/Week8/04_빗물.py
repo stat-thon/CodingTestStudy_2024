7aa240863138188b03d1823d6c3d73152c5fb191
Donghyun Son <69744314+stat-thon@users.noreply.github.com>
2024-07-12 13:55:03 +0900

    upload py

--- Thon/Baekjoon/Week8/04_빗물.py
+++ Thon/Baekjoon/Week8/04_빗물.py
@@ -31,3 +31,24 @@ while g + 1 < r:
     result += cnt
 
 print(result)
+
+
+### 답안 참고
+import sys
+ans = 0
+
+H, W = list(map(int, sys.stdin.readline().split()))
+blocks = list(map(int, sys.stdin.readline().split()))
+
+ans = 0
+
+for i in range(1, W-1):
+    l_max = max(blocks[:i])
+    r_max = max(blocks[i+1:])
+    
+    cmp = min(l_max, r_max)
+    
+    if blocks[i] < cmp:
+        ans += cmp - blocks[i]
+
+print(ans)
