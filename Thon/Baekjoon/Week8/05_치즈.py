b3727e8e61f1bb28bf6b0555fb6606a2ed4d9e3c
Donghyun Son <69744314+stat-thon@users.noreply.github.com>
2024-07-17 14:32:25 +0900

    update py

--- Thon/Baekjoon/Week8/05_치즈.py
+++ Thon/Baekjoon/Week8/05_치즈.py
@@ -52,3 +52,34 @@ while sum(map(sum, cheese)) != 0:
     
 print(t)
 print(cnt)
+
+
+### 참고 답안
+n, m = map(int, input().split())
+cheese = [list(map(int, input().split())) for i in range(n)]
+visit = [list(0 for i in range(m)) for i in range(n)]
+melt = [(0,0)]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+def bfs(melt, time):
+    count = len(melt)
+    find = []
+    while melt:
+        x, y = melt.pop(0)
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if (nx >= 0 and nx < n) and (ny >= 0 and ny < m):
+                if visit[nx][ny] == 0:
+                    visit[nx][ny] = 1
+                    if cheese[nx][ny] == 0:
+                        melt.append((nx, ny))
+                    elif cheese[nx][ny] == 1:
+                        find.append((nx, ny))
+    if find:
+        return bfs(find, time + 1)
+    else:
+        return time, count
+time, count = bfs(melt, 0)
+print(time)
+print(count)
