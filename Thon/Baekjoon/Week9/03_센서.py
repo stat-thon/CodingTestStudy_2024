98ab1d29b1b48b3bccecbbe7a6a112a8075c27b7
thon <stat.thon@gmail.com>
2024-07-24 14:50:27 +0900

    update py

--- Thon/Baekjoon/Week9/03_센서.py
+++ Thon/Baekjoon/Week9/03_센서.py
@@ -28,3 +28,14 @@ def solution(N, K, coord):
     return sum(dist)
 
 print(solution(N, K, coord))
+
+### 다른 답안
+N = int(input())
+K = int(input())
+sensors = list(map(int, input().split()))
+sensors.sort()
+
+sensors_diff = [sensors[i]-sensors[i-1] for i in range(1, N)]
+sensors_diff.sort()
+
+print(sum(sensors_diff[:N-K]))
