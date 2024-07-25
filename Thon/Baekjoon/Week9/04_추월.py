bb2c89d7b3a3f5e78960018564459d995e82b61c
thon <stat.thon@gmail.com>
2024-07-25 14:06:16 +0900

    update py

--- Thon/Baekjoon/Week9/04_추월.py
+++ Thon/Baekjoon/Week9/04_추월.py
@@ -1,4 +1,23 @@
 ### 2002 - 추월
-# 고민: 
-# 코딩: 
+# 고민: 10분
+# 코딩: 30분
 
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+entry = [input() for _ in range(N)]
+out = [input() for _ in range(N)]
+
+cnt = 0
+for i in range(N):
+    out_idx = out.index(entry[i])
+
+    for car in entry[:i]:
+        if car not in out[:out_idx]:
+            cnt += 1
+            break
+
+print(cnt)
+
+# 완전 탐색이라 오래 걸림
