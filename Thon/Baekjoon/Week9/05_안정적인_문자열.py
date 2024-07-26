4d2eb1f17b7a2df57aea1bb86536ff0658c77f6e
thon <stat.thon@gmail.com>
2024-07-26 14:47:14 +0900

    update py

--- Thon/Baekjoon/Week9/05_안정적인_문자열.py
+++ Thon/Baekjoon/Week9/05_안정적인_문자열.py
@@ -1,4 +1,48 @@
 ### 4990 - 안정적인 문자열
-# 고민: 
-# 코딩: 
+# 고민: 20분
+# 코딩: 10분
 
+from collections import deque
+
+def stable(s):
+
+    dq = deque(s)
+    stack = deque()
+
+    while dq:
+        q = dq.popleft()
+
+        if q == '}' and stack and stack[-1] == '{':
+            stack.pop()
+            
+        else:
+            stack.append(q)
+    
+    cnt = 0
+    if not stack:
+        return cnt
+        
+    while stack:
+        q1 = stack.popleft()
+        q2 = stack.popleft()
+
+        if q1 + q2 == '}{':
+            cnt += 2
+        else:
+            cnt += 1
+    
+    return cnt
+
+#import sys
+#input = sys.stdin.readline
+
+num = 1
+while True:
+    s = input()
+
+    if '-' in s:
+        break
+
+    print(f'{num}. {stable(s)}')
+
+    num += 1
